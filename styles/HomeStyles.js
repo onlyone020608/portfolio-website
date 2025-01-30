0a62e37788fb63316e380b5bc791bac4bf0c12d8
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-01-30 09:13:07 +0900

    feat(home): add initial page structure

---
+++ styles/HomeStyles.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MainTitle = styled.div``;
+
+export const SubTitle = styled.div``;
+
+export const DescriptionText = styled.div``;
