54ab5fc69fbcd8af4e4fd84888353b41413d0da3
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-01-30 09:22:41 +0900

    style(home): add basic styles

--- styles/HomeStyles.js
+++ styles/HomeStyles.js
@@ -2,10 +2,22 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const MainTitle = styled.div``;
+export const MainTitle = styled.div`
+  font-size: 75px;
+  font-weight: 400;
+`;
 
-export const SubTitle = styled.div``;
+export const SubTitle = styled.div`
+  font-size: 75px;
+  font-weight: 600;
+`;
 
-export const DescriptionText = styled.div``;
+export const DescriptionText = styled.div`
+  font-size: 26px;
+  font-weight: 400;
+  text-align: center;
+`;
