3d2ac14651de839398ef6bcef261223b424c65f9
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-02-01 08:39:38 +0900

    feat(navbar): add router-based navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,13 +2,29 @@ import styles from "../styles/components/navbar.module.css";
 import { MenuItem } from "../styles/NavBarStyles";
 import globalStyles from "../styles/globals.module.css";
 import classNames from "classnames";
+import { useRouter } from "next/navigation";
+
 export default function NavBar() {
+  const router = useRouter();
+
+  const goToAbout = () => {
+    router.push("/about");
+  };
+
+  const goToHome = () => {
+    router.push("/");
+  };
+
+  const goToProject = () => {
+    router.push("/projects");
+  };
+
   return (
     <div className={classNames(globalStyles.flexRow, styles.navWrapper)}>
       <div className={styles.menuWrapper}>
-        <MenuItem>home</MenuItem>
-        <MenuItem>project</MenuItem>
-        <MenuItem>info</MenuItem>
+        <MenuItem onClick={goToHome}>home</MenuItem>
+        <MenuItem onClick={goToProject}>project</MenuItem>
+        <MenuItem onClick={goToAbout}>info</MenuItem>
       </div>
       <div className={classNames(globalStyles.flexRow, styles.languageWrapper)}>
         <MenuItem>EN</MenuItem>
