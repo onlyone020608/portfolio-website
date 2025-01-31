7624611dd1e57b5c574c12592434545e2cc7bd27
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-01-31 09:08:34 +0900

    feat(about): add tech stack section layout and styles
    - Added tech stack section structure and styles
    - Updated overall about page styles for consistency

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,9 @@
-import { ContactIcon } from "../styles/AboutStyle";
+import {
+  ContactIcon,
+  TechBar,
+  TechBarItem,
+  TechIconWrapper,
+} from "../styles/AboutStyle";
 import globalStyles from "../styles/globals.module.css";
 import classNames from "classnames";
 import styles from "../styles/components/about.module.css";
@@ -6,26 +11,62 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div>
-      <div className={styles.name}>Hyewon Kim</div>
-      <div className={globalStyles.flexRow}>
-        <ContactIcon>
-          <Image src="/github.svg" alt="GitHub Logo" width={54} height={54} />
-          GitHub
-        </ContactIcon>
-        <ContactIcon>
-          <Image
-            src="/linkedin.svg"
-            alt="LinkedIn Logo"
-            width={54}
-            height={54}
-          />
-          LinkedIn
-        </ContactIcon>
-        <ContactIcon>
-          <Image src="/gmail.svg" alt="Gmail Logo" width={54} height={54} />
-          Gmail
-        </ContactIcon>
+    <div className={styles.wrapper}>
+      <div
+        className={classNames(globalStyles.flexRow, styles.bodyUpperWrapper)}
+      >
+        <div
+          className={classNames(
+            globalStyles.flexColumn,
+            styles.bodyUpperLeftWrapper
+          )}
+        >
+          <div className={styles.name}>Hyewon Kim</div>
+          <div
+            className={classNames(globalStyles.flexRow, styles.contactWrapper)}
+          >
+            <ContactIcon>
+              <Image
+                src="/github.svg"
+                alt="GitHub Logo"
+                width={54}
+                height={54}
+              />
+              GitHub
+            </ContactIcon>
+            <ContactIcon>
+              <Image
+                src="/linkedin.svg"
+                alt="LinkedIn Logo"
+                width={54}
+                height={54}
+              />
+              LinkedIn
+            </ContactIcon>
+            <ContactIcon>
+              <Image src="/gmail.svg" alt="Gmail Logo" width={54} height={54} />
+              Gmail
+            </ContactIcon>
+          </div>
+        </div>
+        <div className={styles.description}>
+          Building scalable software and <br></br>solving complex backend
+          challenges.
+        </div>
+      </div>
+      <div className={styles.techTitle}>Tech Stack</div>
+      <div className={styles.techWrapper}>
+        <TechBar>
+          <TechBarItem>Languages</TechBarItem>
+          <TechBarItem>Backend</TechBarItem>
+          <TechBarItem>Frontend</TechBarItem>
+          <TechBarItem>Database</TechBarItem>
+          <TechBarItem>Tools</TechBarItem>
+        </TechBar>
+        <TechIconWrapper>
+          <Image src="/django.svg" alt="Django Logo" width={40} height={40} />
+          <Image src="/spring.svg" alt="Spring Logo" width={40} height={40} />
+        </TechIconWrapper>
       </div>
     </div>
   );
--- styles/AboutStyle.js
+++ styles/AboutStyle.js
@@ -7,3 +7,31 @@ export const ContactIcon = styled.div`
   border: 1px solid red;
   align-items: center;
 `;
+
+export const TechBar = styled.div`
+  width: 1026.33px;
+  height: 59.92px;
+  padding: 2px 83.42px 2px 2px;
+  /* gap: 94.42px; */
+  border-radius: 22px;
+  background-color: #cbcbcb;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const TechBarItem = styled.div`
+  display: flex;
+  background-color: white;
+  border-radius: 22px;
+  font-size: 19.25px;
+  color: #5b5b5b;
+  font-weight: 600;
+  align-items: center;
+  padding: 0px 65px;
+`;
+
+export const TechIconWrapper = styled.div`
+  display: flex;
+  gap: 34px;
+  padding-left: 20px;
+`;
