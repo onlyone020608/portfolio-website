6ddce952aa552fdc7f42721b762fe7cc8a062cbb
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-02-01 13:49:32 +0900

    feat(projects): add links to navigate to individual project pages

--- pages/projects.js
+++ pages/projects.js
@@ -1,18 +1,23 @@
-import { ProjectName } from "../styles/ProjectsStyle";
+import { ProjectName, ProjectLink } from "../styles/ProjectsStyle";
 import globalStyles from "../styles/globals.module.css";
 import classNames from "classnames";
 import styles from "../styles/components/projects.module.css";
+import Link from "next/link";
 
 export default function Projects() {
   return (
     <div className={classNames(globalStyles.flexRow, styles.indexWrapper)}>
       <div>
-        <ProjectName>01</ProjectName>
-        <ProjectName>Grocey</ProjectName>
+        <ProjectLink href="/projects/grocey">
+          <ProjectName>01</ProjectName>
+          <ProjectName>Grocey</ProjectName>
+        </ProjectLink>
       </div>
       <div>
-        <ProjectName>02</ProjectName>
-        <ProjectName>Wiseowl</ProjectName>
+        <ProjectLink href="/projects/wiseowl">
+          <ProjectName>02</ProjectName>
+          <ProjectName>Wiseowl</ProjectName>
+        </ProjectLink>
       </div>
       <div>
         <ProjectName>03</ProjectName>
--- styles/ProjectsStyle.js
+++ styles/ProjectsStyle.js
@@ -1,6 +1,12 @@
 import styled from "@emotion/styled";
-
+import Link from "next/link";
 export const ProjectName = styled.div`
   font-size: 52px;
   font-weight: 600;
+
+  color: black;
+`;
+
+export const ProjectLink = styled(Link)`
+  text-decoration: none;
 `;
