53ae0c50e03a48bd02c7e393cd7ab5a58d2cf80f
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-02-01 09:21:32 +0900

    refactor(navbar): replace router-based navigation with Link and update styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,11 @@ import styles from "../styles/components/navbar.module.css";
 import { MenuItem } from "../styles/NavBarStyles";
 import globalStyles from "../styles/globals.module.css";
 import classNames from "classnames";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 export default function NavBar() {
   const router = useRouter();
+  const pathname = usePathname();
 
   const goToAbout = () => {
     router.push("/about");
@@ -18,17 +19,23 @@ export default function NavBar() {
   const goToProject = () => {
     router.push("/projects");
   };
-
   return (
     <div className={classNames(globalStyles.flexRow, styles.navWrapper)}>
       <div className={styles.menuWrapper}>
-        <MenuItem onClick={goToHome}>home</MenuItem>
-        <MenuItem onClick={goToProject}>project</MenuItem>
-        <MenuItem onClick={goToAbout}>info</MenuItem>
+        {/* <MenuItem onClick={goToHome}>home</MenuItem> */}
+        <MenuItem href="/" active={pathname === "/"}>
+          home
+        </MenuItem>
+        <MenuItem href="/projects" active={pathname === "/projects"}>
+          project
+        </MenuItem>
+        <MenuItem href="/about" active={pathname === "/about"}>
+          info
+        </MenuItem>
       </div>
       <div className={classNames(globalStyles.flexRow, styles.languageWrapper)}>
-        <MenuItem>EN</MenuItem>
-        <MenuItem>KR</MenuItem>
+        {/* <MenuItem>EN</MenuItem>
+        <MenuItem>KR</MenuItem> */}
       </div>
     </div>
   );
--- styles/NavBarStyles.js
+++ styles/NavBarStyles.js
@@ -1,6 +1,10 @@
 import styled from "@emotion/styled";
-
-export const MenuItem = styled.div`
+import Link from "next/link";
+export const MenuItem = styled(Link)`
   font-size: 24px;
   font-weight: 600;
+  cursor: pointer;
+  padding: 5px;
+  text-decoration: none;
+  color: ${(props) => (props.active ? "black" : "#cbcbcb")};
 `;
