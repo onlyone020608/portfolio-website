751bf78d6fc54b22a42396193e489e300809171d
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-02-06 09:04:43 +0900

    style(projects): add hover transition effect to project menu items

--- styles/ProjectsStyle.js
+++ styles/ProjectsStyle.js
@@ -3,10 +3,16 @@ import Link from "next/link";
 export const ProjectName = styled.div`
   font-size: 52px;
   font-weight: 600;
-
   color: black;
 `;
 
 export const ProjectLink = styled(Link)`
   text-decoration: none;
+  div {
+    transition: color 0.3s ease, transform 0.3s ease;
+  }
+  &:hover div {
+    color: #5b5b5b;
+    transform: scale(1.03);
+  }
 `;
